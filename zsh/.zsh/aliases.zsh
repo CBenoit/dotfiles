88a6d555fd43abecf63db438851434d6c95bd451
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-09-07 15:22:39 -0400

    Update zsh aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,24 +1,34 @@
-alias la='ls --group-directories-first --color=auto -F --almost-all'
-alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias grep='grep --color=tty -d skip -n'
 alias cp="cp -i"    # confirm before overwriting something
-alias da='java -jar /media/data/cloud/dev/Java/diskanalyzer/target/diskanalyzer-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
 alias fixpacman='sudo rm -f /var/lib/pacman/db.lck && sudo pacman-mirrors -g && sudo pacman-key --refresh'
 alias alarmit='cvlc --play-and-stop --play-and-exit --quiet /media/data/cloud/media/sounds/notif2.wav'
 alias pscheduler='autocmd -i 300 "cvlc --play-and-stop --play-and-exit --quiet /media/data/cloud/media/sounds/notif2.wav"'
 alias df='df -h'    # Human-readable sizes
 alias free='free -m'    # Show sizes in MB
 alias latexmk='latexmk -lualatex -interaction=batchmode -silent'
+alias fd='fd --follow --hidden'
+alias kak='tmux new-session kak'
+alias authy="/usr/bin/electron --app /usr/lib/authy/app.asar"
+
+if (( $+commands[exa] )) ; then
+	alias ls='exa --group-directories-first --time-style=iso --color=auto -F --git'
+else
+	alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+fi
+
+if (( $+commands[bat] )) ; then
+	alias cat='bat'
+fi
 
 # fix buggy opera buggy colors with black themes.
 alias opera='env GTK2_RC_FILES=/usr/share/themes/Numix/gtk-2.0/gtkrc opera'
 
 function steam() {
-    LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' /usr/bin/steam
+	LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' /usr/bin/steam
 }
 
 md() {
-    pandoc -s -f markdown -t man "$*" | man -l -
+	pandoc -s -f markdown -t man "$*" | man -l -
 }
 
 alias proxy='export http_proxy=http://proxy.utbm.fr:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;export NO_PROXY="local-delivery,local-auth";export no_proxy=$NO_PROXY'
