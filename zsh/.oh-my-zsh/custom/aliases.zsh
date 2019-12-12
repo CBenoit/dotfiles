54f14db3b0462e5ea7b98a3f090f90dce165dc0b
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-12-11 19:57:55 -0500

    zsh: cleanup

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,21 +1,12 @@
 alias grep='grep --color=tty -d skip -n'
 alias cp="cp -i"    # confirm before overwriting something
 alias fixpacman='sudo rm -f /var/lib/pacman/db.lck && sudo pacman-mirrors -g && sudo pacman-key --refresh'
-alias alarmit='cvlc --play-and-stop --play-and-exit --quiet /media/data/cloud/media/sounds/notif2.wav'
-alias pscheduler='autocmd -i 300 "cvlc --play-and-stop --play-and-exit --quiet /media/data/cloud/media/sounds/notif2.wav"'
-alias df='df -h'    # Human-readable sizes
-alias free='free -m'    # Show sizes in MB
+alias df='df -h'    # human-readable sizes
+alias free='free -m'    # show sizes in MB
 alias latexmk='latexmk -lualatex -interaction=batchmode -silent'
 alias fd='fd --follow --hidden'
 alias kak='tmux new-session kak'
-alias authy="/usr/bin/electron --app /usr/lib/authy/app.asar"
-alias dolphin='DESKTOP_SESSION=kde dolphin'
-alias okular='DESKTOP_SESSION=kde okular'
 alias scrotclip='scrot -s /tmp/tmpscrot.png && xclip -selection c -t image/png /tmp/tmpscrot.png'
-alias virtualscn='intel-virtual-output && xrandr --output eDP1 --auto --output VIRTUAL1 --auto --right-of eDP1'
-
-alias proxy='export http_proxy=http://proxy.utbm.fr:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;export NO_PROXY="local-delivery,local-auth";export no_proxy=$NO_PROXY'
-alias noproxy='unset http_proxy;unset HTTPS_PROXY;unset HTTP_PROXY;unset FTP_PROXY;unset https_proxy;unset ftp_proxy'
 
 if (( $+commands[exa] )) ; then
 	alias ls='exa --group-directories-first --time-style=iso --color=auto -F --git'
@@ -26,15 +17,6 @@ fi
 if (( $+commands[bat] )) ; then
 	alias cat='bat'
 fi
-
-function steam() {
-	LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' /usr/bin/steam
-}
-
-markdown() {
-	pandoc -s -f markdown -t man "$*" | man -l -
-}
-alias md="markdown"
 
 # inspired from: https://github.com/cquery-project/cquery/wiki/Compilation-database
 gen_compile_commands() {
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/home/auroden/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -79,10 +79,8 @@ plugins=(
 	cp
 	extract
 	encode64
-	last-working-dir
 	rsync
 	docker
-	web-search
 	zsh-syntax-highlighting
 	cargo
 	rust
@@ -90,28 +88,3 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
