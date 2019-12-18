de35a11244ba61385583ffbeb11cc5e79556db50
Benoît CORTIER <benoit.cortier@fried-world.eu>
2019-12-18 15:20:04 -0500

    zsh: update env-vars.zsh

--- zsh/.oh-my-zsh/custom/env-vars.zsh
+++ zsh/.oh-my-zsh/custom/env-vars.zsh
@@ -9,3 +9,11 @@ export CARGO_HOME=~/.cargo
 export PATH=$PATH:$CARGO_HOME/bin
 export PATH=$PATH:/snap/bin
 export PATH=$PATH:~/.local/bin
+
+# workstation
+# export IS_WORKSTATION=1
+
+# wallpapers path
+export WALLPAPERS_DIRECTORY=/data/media/images/wallpapers/
+# export WALLPAPERS_DIRECTORY=/home/$USER/Pictures/wallpapers/
+
