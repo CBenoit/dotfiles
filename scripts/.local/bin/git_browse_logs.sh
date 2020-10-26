6addb7f3a2b8e19a776eb911990a9e5fa0639253
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-10-26 13:53:45 -0400

    scripts: improve git_browse_logs.sh

--- scripts/.local/bin/git_browse_logs.sh
+++ scripts/.local/bin/git_browse_logs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
+
+if [[ $1 == "multi" ]]; then
+  other_args="--multi --bind tab:toggle"
+else
+  other_args=""
+fi
+
 export SKIM_DEFAULT_OPTIONS='--tiebreak index,score --color=current_bg:24'
 export FZF_DEFAULT_OPTS='--tiebreak index --color=bg+:24'
-HASH=$(git log --oneline --decorate --color=always --max-count 200 | $FINDER --ansi -d" " --preview "git show --color=always {1}")
-echo $HASH | cut -d" " -f1
+HASH=$(git log --oneline --decorate --color=always --max-count 200 | $FINDER --ansi -d" " $other_args --preview "git show --color=always {1}")
+echo "$HASH" | cut -d" " -f1
