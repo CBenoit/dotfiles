e5c3d06bdd82102e389423eac9fd7f2dccf85bf2
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-03-02 11:44:53 -0500

    Add basic_setup.sh

---
+++ basic_setup.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+set -euf -o pipefail
+
+## check some dependencies
+
+if ! command -v stow &> /dev/null; then
+	echo "stow is missing"
+	exit
+fi
+
+if ! command -v git &> /dev/null; then
+	echo "stow is missing"
+	exit
+fi
+
+if ! command -v curl &> /dev/null; then
+	echo "stow is missing"
+	exit
+fi
+
+## easy stows
+stow -t ~ -S git alacritty neovim powershell i3 i3blocks rofi
+
+## no folding stows
+stow -t ~ --no-folding -S scripts
+
+## zsh
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# syntax highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md#oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# stow config
+stow -t ~ --no-folding -S ./zsh/
+
+## rust toolchain
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## zoxide
+~/.cargo/bin/cargo install zoxide
+
+## starship
+~/.cargo/bin/cargo install starship
+
