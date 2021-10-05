3556247a9d53af5a447b37fc88e0b9c1f05a2e01
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-10-05 09:32:39 -0400

    Update Anki

--- install_anki.sh
+++ install_anki.sh
@@ -6,7 +6,7 @@ if ! command -v curl &> /dev/null; then
 	exit
 fi
 
-VERSION="2.1.46"
+VERSION="2.1.48"
 curl -L "https://github.com/ankitects/anki/releases/download/$VERSION/anki-$VERSION-linux.tar.bz2" --output "anki-$VERSION-linux.tar.bz2"
 tar xf "anki-$VERSION-linux.tar.bz2" --directory=$HOME/bin/
 rm "anki-$VERSION-linux.tar.bz2"
