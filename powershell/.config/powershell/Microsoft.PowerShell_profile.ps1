2324090c645b259e8a1dbb865a9214f0b0373f32
Benoît CORTIER <bcortier@pm.me>
2022-05-03 13:53:06 -0400

    powershell: update config

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,8 @@
-Invoke-Expression (&starship init powershell)
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Enable IntelliSense based on history
+Set-PSReadLineOption -PredictionSource History
+
+# Starship prompt
+Invoke-Expression (starship init powershell)
