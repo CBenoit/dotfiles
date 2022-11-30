4fd235bf3cf20b6288a9d38d55b588e2d52714be
Benoît CORTIER <bcortier@proton.me>
2022-11-30 09:23:22 -0500

    pwsh: hx alias + zoxide support

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,5 +4,18 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 # Enable IntelliSense based on history
 Set-PSReadLineOption -PredictionSource History
 
+# Define default editor
+$Env:EDITOR = "hx"
+$Env:GIT_EDITOR = $Env:EDITOR
+
+# Editor alias
+New-Alias -Name e -Value $Env:EDITOR
+
 # Starship prompt
 Invoke-Expression (starship init powershell)
+
+# Zoxide
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
