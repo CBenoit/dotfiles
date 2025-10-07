f64b2e91fda5231fa0bf08422f70648fc6b4ae8f
Benoît CORTIER <git.divisible626@passmail.com>
2025-10-08 08:19:47 +0900

    .

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,3 +1,10 @@
-return {
-  hide_tab_bar_if_only_one_tab = true,
-}
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+config.default_prog = { '/home/auroden/.cargo/bin/nu' }
+
+-- Finally, return the configuration to wezterm:
+return config
